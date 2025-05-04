9832b7c967c99a0902f1d18497605b9b07440893
Zaqpurpur-Neo <baristaunescio@gmail.com>
2025-05-04 11:11:15 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $name = "arap";
 $jurusan = "industri";
+$nim = "H1D024051"
 ?>
 
 <html>
@@ -9,6 +10,7 @@ $jurusan = "industri";
 	</head>
 	<body>
 		<h1>Nama: <?php echo $name ?></h1>
+		<h1>Nim: <?php echo $nim ?></h1>
 		<h1>Jurusan: <?php echo $jurusan ?></h1>
 	</body>
 </html>
